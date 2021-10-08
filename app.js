7ac7060beab22fc6ea75631a169774cf230d7d5b
Alejandro Cuello De Moya <acuellodemoya@gmail.com>
2021-10-08 15:44:59 -0500

    Esquema creado

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const URI = 'mongodb+srv://root:12345@cluster0.vtpki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected'))
+.catch(err => console.log(`Error: \n ${err}`));
+
 
 app.get('/', (req, res) => {
     console.log('Hello, world!');
---
+++ models/doctorModel.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const monitorSchema = new  mongoose.Schema({
+    
+});
